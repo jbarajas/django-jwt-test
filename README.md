1d62a251631cbc27313817d9335f2327793825fd
jose.barajas <jose.barajas@attackiq.com>
2019-01-09 14:24:40 -0800

    First commit:

---
+++ README.md
@@ -0,0 +1,9 @@
+# django-jwt-test
+
+A Django project with JWT authentication for testing.
+
+username: admin
+password: password
+
+
+`python manage.py runserver`
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/josebarajas/Downloads/cookbook-master/_articles/redux_jwt_auth/backend/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
