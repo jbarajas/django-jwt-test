e951984f72b72d7a51cf7a70a8d1c3f4625f4bb6
jose.barajas <jose.barajas@attackiq.com>
2019-01-09 14:27:50 -0800

    Clean up

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/josebarajas/Downloads/cookbook-master/_articles/redux_jwt_auth/backend/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
